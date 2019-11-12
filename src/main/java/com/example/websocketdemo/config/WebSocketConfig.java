9ba7a80f9561a35747e6311f4d4a68e3e6566359
Cooper.Wu <wutuo@chinamobile.com>
2019-11-12 11:13:55 +0800

    websocket监听

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -16,6 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").addInterceptors(new HttpHandShakeInterceptor()).setAllowedOrigins("*").withSockJS();
+//        registry.addEndpoint("/ws").withSockJS();
     }
 
     @Override
